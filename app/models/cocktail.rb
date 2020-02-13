f88be760371ce06dde7f6744916d4dae4a95ba04
Guillaume Protard <guillaumeprotard@gmail.com>
2020-02-13 16:38:42 +0100

    Styling WIP index and show

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,5 +2,5 @@ class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
   validates :name, uniqueness: true
-  validates :name, presence: true
+  validates :name, :photo_url, presence: true
 end
