769a3caa96ffe5e257659e5b685141c1e2cde712
Guillaume Protard <guillaumeprotard@gmail.com>
2020-02-14 12:47:08 +0100

    Built seeds based on API

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
 
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
-Ingredient.create(name: "rhum")
-Ingredient.create(name: "gin")
+puts "Cleaning database..."
+Cocktail.destroy_all
+Ingredient.destroy_all
+
+puts "Creating cocktails"
+
+10.times do |cocktail|
+  url = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
+  cocktail_serialized = open(url).read
+  cocktail = JSON.parse(cocktail_serialized)
+  cocktail_attributes = cocktail['drinks'][0]
+  cocktail_name = cocktail_attributes['strDrink']
+  cocktail_photo_url = cocktail_attributes['strDrinkThumb']
+  cocktail = Cocktail.create(name: cocktail_name, photo_url: cocktail_photo_url)
+  puts "Created #{cocktail.name}"
+end
+
+puts "Finished"
+
+puts "-----------------------------------------------"
+
+ingredients_url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_serialized = open(ingredients_url).read
+ingredients = JSON.parse(ingredients_serialized)
+
+puts "Creating Ingredients"
+
+i = 0
+while i < 10
+  ingredient_name = ingredients['drinks'][i]['strIngredient1']
+  ingredient = Ingredient.create(name: ingredient_name)
+  puts "Created #{ingredient.name}"
+  i += 1
+end
+
+puts "Finished"
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+# Ingredient.create(name: "rhum")
+# Ingredient.create(name: "gin")
